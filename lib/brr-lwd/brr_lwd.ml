a1e5363bdee8a3b9d9afc5659c9a63ae8e37f1cc
Patrick Ferris <pf341@patricoferris.com>
2021-10-16 21:14:08 +0100

    add some startup helpers

---
+++ lib/brr-lwd/brr_lwd.ml
@@ -0,0 +1,18 @@
+open Brr
+module Elwd = Elwd
+
+let onload v = 
+  let onload' _ = 
+    let root = Lwd.observe @@ v in
+    let on_invalidate _ = 
+      ignore @@ G.request_animation_frame @@ fun _ -> ignore (Lwd.quick_sample root)
+    in
+    Lwd.set_on_invalidate root on_invalidate;
+    El.append_children (Document.body G.document) @@ Lwd.quick_sample root
+  in
+  ignore (Fut.map onload' Ev.(next load @@ Window.as_target G.window))
+
+module Let_syntax = struct 
+  let (let+) a f = Lwd.map ~f a
+  let (let*) a f = Lwd.bind ~f a
+end
